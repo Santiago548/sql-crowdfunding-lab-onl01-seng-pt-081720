a0b57e474732f5c98e379a6711db98bdedff2a77
Dominic Barajas <dominicbarajas548@gmail.com>
2020-10-17 20:44:27 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,8 +1,1 @@
-"CREATE TABLE projects (
-  id INTEGER PRIMARY KEY,
-  title TEXT,
-  category TEXT,
-  funding_goal INTEGER,
-  start_date INTEGER,
-  end_date INTEGER,
-);"
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date INTEGER, end_date INTEGER);
